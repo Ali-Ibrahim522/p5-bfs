7bf47d7c66bac527c579745782f3d2291235b36e
Ali-Ibrahim522 <52763138+Ali-Ibrahim522@users.noreply.github.com>
2023-03-12 08:09:07 +0000

    one more time :cat:

--- fs.h
+++ fs.h
@@ -6,10 +6,8 @@
 // ===================================================================
 
 #include <stdio.h>
-#include <stdlib.h>
 #include "alias.h"
 #include "errors.h"
-#include "stdbool.h"
 
 i32 fsClose (i32 fd);
 i32 fsCreate(str name);
